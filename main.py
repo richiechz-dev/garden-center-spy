d6b3a57b657a79d998a10926ebcaf8ac99f06408
Ricardo Chavez <gic.rc455@gmail.com>
2026-08-22 13:34:31 -0600

    fix: correct api url to fetch by catalogId equal to 10101 = follaje with price range sequence

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from extractors.home_depot import HomeDepot
+from load.load import load_products
 
 if __name__ == "__main__":
     home_depot = HomeDepot(
-        "https://www.homedepot.com.mx/search/resources/api/v2/products?storeId=10351&searchTerm=kwPlantasComestibles&limit=28&offset=0&contractId=4000000000000000003&currency=MXN&langId=-5&marketId=21&stLocId=12605&extendedCatalog=false&marketOnly=true&physicalStoreId=8702&profileName=HCL_V2_findProductsBySearchTermWithPrice&selectedFacets=%5Bobject+Object%5D&minPrice=-1&maxPrice=-1&selectedPageOffset=0&orderBy=0"
+        "https://www.homedepot.com.mx/search/resources/api/v2/products?offset=0&limit=28&marketId=290&stLocId=12526&physicalStoreId=8774&orderBy=5&storeId=10351&catalogId=10101&profileName=HCL_V2_findProductsByCategoryWithPriceRangeSequenceTest&langId=-5&contractId=4000000000000000003&currency=MXN&categoryId=11156"
     )
 
     products = home_depot.run()
-    print(products)
+    load_products(products)
+    print(f"Se han cargado {len(products)} productos")
